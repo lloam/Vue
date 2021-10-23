77a6be5e29fac6693015a3e8d28e8ca53f26ea33
lloam <2196143404@qq.com>
2021-10-23 15:59:43 +0800

    :art:单文件组件

---
+++ vue_basic/19_单文件组件/Student.vue
@@ -0,0 +1,28 @@
+<template>
+    <!-- 组建的结构 -->
+    <div>
+        <h2>学校姓名：{{name}}</h2>
+        <h2>学校地址：{{age}}</h2>
+        <button @click="showName">点我提示学校名</button>
+    </div>
+</template>
+
+<script>
+    // 组件交互相关的代码(数据\方法等等)
+    export default {
+        name: 'Student',
+        data() {
+            return {
+                name: '张三',
+                age: 18
+            }
+        },
+        methods: {
+            showName(){
+                alert(this.name)
+            }
+        },
+    }
+</script>
+
+
