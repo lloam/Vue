2f6bcbd00d2a18e9bb9446b34b07df9c8d537525
lloam <2196143404@qq.com>
2021-10-23 23:28:38 +0800

    :art:mixin混入（合）

---
+++ vue_test/04_src_mixin混入（合）/components/School.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+      <h2 @click="showName">学校名称：{{name}}</h2>
+      <h2>学校地址：{{sex}}</h2>
+  </div>
+</template>
+
+<script>
+// import {mixin,mixin2} from '../mixin'
+export default {
+    name: 'School',
+    data(){
+        return {
+            name: '南昌大学',
+            sex: '江西南昌'
+        }
+    },
+    // mixins: [mixin,mixin2]
+}
+</script>
---
+++ vue_test/04_src_mixin混入（合）/components/Student.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+      <h2 @click="showName">学生姓名：{{name}}</h2>
+      <h2>学生性别：{{sex}}</h2>
+  </div>
+</template>
+
+<script>
+// import {mixin,mixin2} from '../mixin'
+export default {
+    name: 'Student',
+    data(){
+        return {
+            name: '张三',
+            sex: '男',
+            x: 666
+        }
+    },
+    // mixins: [mixin,mixin2],
+    /* mounted() {
+        console.log('你好啊！！！！！！！！！')
+    }, */
+}
+</script>
