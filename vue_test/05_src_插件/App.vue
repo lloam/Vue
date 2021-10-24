c9ab1161c56d8a054dcd7e21945f664ec7a737ef
lloam <2196143404@qq.com>
2021-10-24 10:14:16 +0800

    :art:插件

---
+++ vue_test/05_src_插件/App.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <Student/>
+        <School/>
+    </div>
+</template>
+
+<script>
+    // 引入 School 组件
+    import Student from './components/Student.vue'
+    import School from './components/School.vue'
+    export default {
+        name: 'App',
+        components: {
+            Student,
+            School
+        }
+    }
+</script>
