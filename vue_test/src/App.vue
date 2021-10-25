56ab24ae9a928b4f71862fcb9c8a35aedc41e8a8
lloam <2196143404@qq.com>
2021-10-25 23:12:36 +0800

    :bug:TodoList使用事件总线替换App向item

--- vue_test/src/App.vue
+++ vue_test/src/App.vue
@@ -1,31 +1,127 @@
 <template>
-  <div class="app">
-    <School />
-    <Student />
+  <div id="root">
+    <div class="todo-container">
+      <div class="todo-wrap">
+        <MyHeader @addTodo="addTodo" />
+        <MyList :todos="todos" />
+        <MyFooter
+          :todos="todos"
+          @checkAllTodo="checkAllTodo"
+          @clearAllTodo="clearAllTodo"
+        />
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 // 引入 School 组件
-import Student from './components/Student.vue'
-import School from './components/School.vue'
+import MyHeader from './components/MyHeader.vue'
+import MyList from './components/MyList.vue'
+import MyFooter from './components/MyFooter.vue'
 export default {
   name: 'App',
   components: {
-    Student,
-    School,
+    MyHeader,
+    MyList,
+    MyFooter,
   },
   data() {
     return {
-      msg: '你好啊！',
-      studentName: '',
+      todos: JSON.parse(localStorage.getItem('todos')) || [],
     }
+  },
+  methods: {
+    // 添加一个 头都
+    addTodo(todoObj) {
+      this.todos.unshift(todoObj)
+    },
+    // 勾选or取消勾选一个 todo
+    checkTodo(id) {
+      this.todos.forEach((todo) => {
+        if (todo.id === id) todo.done = !todo.done
+      })
+    },
+    // 删除一个 todo
+    deleteTodo(id) {
+      this.todos = this.todos.filter((todo) => {
+        return todo.id !== id
+      })
+    },
+    // 全选or取消全选
+    checkAllTodo(done) {
+      this.todos.forEach((todo) => {
+        todo.done = done
+      })
+    },
+    // 清除所有已经完成的todo
+    clearAllTodo() {
+      this.todos = this.todos.filter((todo) => {
+        return !todo.done
+      })
+    },
+  },
+  watch: {
+    todos: {
+      // 深度监视，勾选修改了 todo 对象里面的属性，简单的监视检测不到
+      deep: true,
+      handler(value) {
+        localStorage.setItem('todos', JSON.stringify(value))
+      },
+    },
+  },
+  mounted() {
+    this.$bus.$on('checkTodo', this.checkTodo)
+    this.$bus.$on('deleteTodo', this.deleteTodo)
+  },
+  beforeDestroy() {
+    this.$bus.$off('checkTodo', this.checkTodo)
+    this.$bus.$off('deleteTodo', this.deleteTodo)
   },
 }
 </script>
 <style>
-.app {
-  background-color: gray;
-  padding: 5px;
+/*base*/
+body {
+  background: #fff;
+}
+
+.btn {
+  display: inline-block;
+  padding: 4px 12px;
+  margin-bottom: 0;
+  font-size: 14px;
+  line-height: 20px;
+  text-align: center;
+  vertical-align: middle;
+  cursor: pointer;
+  box-shadow: inset 0 1px 0 rgba(255, 255, 255, 0.2),
+    0 1px 2px rgba(0, 0, 0, 0.05);
+  border-radius: 4px;
+}
+
+.btn-danger {
+  color: #fff;
+  background-color: #da4f49;
+  border: 1px solid #bd362f;
+}
+
+.btn-danger:hover {
+  color: #fff;
+  background-color: #bd362f;
+}
+
+.btn:focus {
+  outline: none;
+}
+
+.todo-container {
+  width: 600px;
+  margin: 0 auto;
+}
+.todo-container .todo-wrap {
+  padding: 10px;
+  border: 1px solid #ddd;
+  border-radius: 5px;
 }
 </style>
