c31d18d4952013a09c34c35d848bc89c0caae228
lloam <2196143404@qq.com>
2021-10-26 23:02:09 +0800

    :art:配置代理服务器

--- vue_test/vue.config.js
+++ vue_test/vue.config.js
@@ -5,5 +5,27 @@ module.exports = {
         entry: 'src/main.js',
       },
     },
-    lintOnSave: false   // 关闭语法检查
+    lintOnSave: false,   // 关闭语法检查
+    // 开启代理服务器（方式一）
+    /* devServer: {
+      proxy: 'http://localhost:5000'
+    } */
+    // 开启代理服务器（方式二）
+    devServer: {
+      proxy: {
+        '/lloam': {
+          target: 'http://localhost:5000',
+          pathRewrite:{'^/lloam':''}, // 重写请求地址，正则表达式
+          ws: true, // 用于支持 websocket
+          changeOrigin: true  // 用于控制请求头中的 host 值
+        },
+        '/demo': {
+          target: 'http://localhost:5001',
+          pathRewrite:{'^/demo':''}, // 重写请求地址，正则表达式
+          ws: true, // 用于支持 websocket
+          changeOrigin: true  // 用于控制请求头中的 host 值
+        }
+      }
+    }
+    
   }
