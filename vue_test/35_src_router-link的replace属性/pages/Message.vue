67bac6d33c7952576fddd9e9dd776366b81fd80a
lloam <2196143404@qq.com>
2021-10-31 16:21:41 +0800

    :art:router-link的replace属性

---
+++ vue_test/35_src_router-link的replace属性/pages/Message.vue
@@ -0,0 +1,43 @@
+<template>
+  <div>
+    <ul>
+      <li v-for="message in messageList" :key="message.id">
+        <!-- 跳转路由并携带 query 参，to 的字符串写法 -->
+        <!-- <router-link
+          :to="`/home/message/detail/${message.id}/${message.title}`"
+          >{{ message.title }}</router-link
+        >&nbsp;&nbsp; -->
+        <!-- 跳转路由并携带 query 参，to 的对象写法 -->
+        <router-link
+          :to="{
+            name: 'xiangqing',
+            params: {
+              id: message.id,
+              title: message.title,
+            },
+          }"
+          >{{ message.title }}</router-link
+        >&nbsp;&nbsp;
+      </li>
+    </ul>
+    <hr />
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Message',
+  data() {
+    return {
+      messageList: [
+        { id: '001', title: '消息001' },
+        { id: '002', title: '消息002' },
+        { id: '003', title: '消息003' },
+      ],
+    }
+  },
+}
+</script>
+
+<style></style>
